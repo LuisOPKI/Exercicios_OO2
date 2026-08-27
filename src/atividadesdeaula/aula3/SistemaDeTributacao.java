b4501d22b7eab23c7e7c5f1aaa04b65b76f0cf2e
luis <luis@luis-Aspire-ES1-572>
2026-08-27 14:39:05 -0300

    Começo do Uso de interfaces

---
+++ src/atividadesdeaula/aula3/SistemaDeTributacao.java
@@ -0,0 +1,11 @@
+package atividadesdeaula.aula3;
+
+public class SistemaDeTributacao {
+	private Double valorDoBem;
+
+	public SistemaDeTributacao(Double valorDoBem) {
+		this.valorDoBem = valorDoBem;
+	}
+	public ca
+	
+}
