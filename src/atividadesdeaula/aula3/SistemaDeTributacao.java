743e293b384e21e0a0631316642ad5a5274ca1a6
luis <luis@luis-Aspire-ES1-572>
2026-09-03 14:01:47 -0300

    Uso de interface
    Para o sistema proposto e atividade, sendo diferentes tipos de impostos

--- src/atividadesdeaula/aula3/SistemaDeTributacao.java
+++ src/atividadesdeaula/aula3/SistemaDeTributacao.java
@@ -2,16 +2,15 @@ package atividadesdeaula.aula3;
 
 public class SistemaDeTributacao {
 	private Double valorDoBem;
-	private Imposto imposto;
+	private Imposto imposto; 
 
 	public SistemaDeTributacao(Double valorDoBem, Imposto imposto) {
 		this.valorDoBem = valorDoBem;
 		this.imposto = imposto;
 	}
-
-
-	public void calcularImposto(){
-		System.out.println("O valor do imposto a ser pago pelo seu bem é " + this.imposto.calcularImposto(valorDoBem));
+	public Double Imposto() {
+		Double valorImposto = this.imposto.calcularImposto(valorDoBem);
+		return valorImposto;
 	}
 	
 }
--- src/atividadesdeaula/aula3/SistemaDeTributacaoTeste.java
+++ src/atividadesdeaula/aula3/SistemaDeTributacaoTeste.java
@@ -2,8 +2,13 @@ package atividadesdeaula.aula3;
 
 public class SistemaDeTributacaoTeste {
 	public static void main(String[] args) {
-		SistemaDeTributacao sis = new SistemaDeTributacao(10000.00, new Ipva());
-		
-		sis.calcularImposto();
+		SistemaDeTributacao sis = new SistemaDeTributacao(10000.00, new Irpf());
+		System.out.println("O valor do imposto é: " + sis.Imposto());
+		SistemaDeTributacao sas = new SistemaDeTributacao(10000.00, new Ipva());
+		System.out.println("O valor do imposto é: " + sas.Imposto());
+		SistemaDeTributacao ses = new SistemaDeTributacao(10000.00, new Iof());
+		System.out.println("O valor do imposto é: " + ses.Imposto());
+		SistemaDeTributacao sos = new SistemaDeTributacao(10000.00, new Inss());
+		System.out.println("O valor do imposto é: " + sos.Imposto());
 	}
 }
