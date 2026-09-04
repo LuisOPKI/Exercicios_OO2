2461ff75f2ef1532be5a96912a4ca687290c7aa5
luis <luis@luis-Aspire-ES1-572>
2026-09-04 14:55:38 -0300

    Interface e Arrow

--- src/atividadesdeaula/aula3/ProcessadorTeste.java
+++ src/atividadesdeaula/aula3/ProcessadorTeste.java
@@ -2,6 +2,8 @@ package atividadesdeaula.aula3;
 
 public class ProcessadorTeste {
 	public static void main(String[] args) {
-		
+		ProcessadorNome retorna = ("João da Silva") -> {
+			String nome = 
+		}
 	}
 }
